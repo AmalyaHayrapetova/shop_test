01e8b0533675282cd82ec112ccaa99f3ae99fffa
Amalya Hayrapetova <alya.hayrapetova@gmail.com>
2020-05-18 22:45:23 +0400

    added fonts

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,38 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Heading from "./Header";
 import Banner from "./Banner";
 import Footer from "./Footer";
-import SignUp from "./SignUp"
-import SignIn from "./SignIn"
-import ShoppingCart from "./ShoppingCart"
-import Cards from "./Cards"
-import BlogCards from "./BlogCards"
-import AboutUsPage from "./AboutUsPage"
-import Checkout from "./Checkout"
-// import Blog from "./src/Blog"
-import Routes from './Routes';
+import Cards from "./Cards";
+import BlogCards from "./BlogCards";
+import Routes from "./Routes";
 
-class  App extends React.Component {
-
+class App extends React.Component {
   render() {
-  return (
-    // <body
-    <div className="App">
-       <Heading/> 
-             <Banner/>
-             {/* <AboutUsPage/> */}
-             {/* <Checkout /> */}
-      {/* <ShoppingCard/> */}
-      <Cards/>
-      <BlogCards/>
-      {/* <SignUp/> */}
-      {/* <SignIn/> */}
-      <Footer />
-{/* <Routes/> */}
-    </div>
-
-  );
-}
+    return (
+      // <body
+      <div className="App">
+        <Heading />
+        <Banner />
+        <Cards />
+        <BlogCards />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ shop/src/ProductsList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./css/product_list";
+
+class ProductsList extends React.Component {
+  render() {
+    return (
+      <div className="ProductsContainer">
+        <div className="FilterContainer">
+          <div className="DesktopFilter"></div>
+        </div>
+        <div className = "ProductLists">
+
+        </div>
+      </div>
+    );
+  }
+}
